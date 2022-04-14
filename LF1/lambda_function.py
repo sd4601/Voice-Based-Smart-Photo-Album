9b10b70b234af6e9d681bc322394072352fddf07
Shanthi Sree Devaraju <91554446+sd4601@users.noreply.github.com>
2022-04-14 14:31:09 -0400

    Update lambda_function.py

--- LF1/lambda_function.py
+++ LF1/lambda_function.py
@@ -4,7 +4,8 @@ import urllib.parse
 import requests
 import base64
 from requests_aws4auth import AWS4Auth
-#hey testing
+
+#hey testing new changes
 # credentials = boto3.Session().get_credentials()
 # awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
